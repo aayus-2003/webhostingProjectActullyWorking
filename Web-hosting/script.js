2f0f18d3c54e0ddc108d105771e2fefa41fa3704
Ayush <139258276+aayus-2003@users.noreply.github.com>
2024-04-19 19:40:11 +0000

    Pending changes exported from your codespace

---
+++ Web-hosting/script.js
@@ -0,0 +1,7 @@
+let web = document.querySelectorAll('.web')
+
+web.forEach((value) => {
+    value.addEventListener('click', () => {
+        location.href = 'https://web-hosting-designing.com/index.php'
+    })
+})
